16dffd8913e371a50e9bacd4af024b12eca97bc2
Omar Sherief <omarsherief@Omars-MacBook-Pro.local>
2017-11-16 01:50:38 -0800

    updating deletable

--- FIRDeletable.swift
+++ FIRDeletable.swift
@@ -3,19 +3,20 @@ import Firebase
 
 protocol FIRDeletable
 {
-	static var COLLECTION_NAME: String { get }
+    static var COLLECTION_NAME: String { get }
 }
 
 extension FIRDeletable where Self: FIRModel
 {
-	func delete(completion: ((Error?) -> Void)? = nil)
-	{
-		FIRDatabase.database()
-			.reference(withPath: Self.COLLECTION_NAME)
-			.child(self.key)
-			.removeValue { (e: Error?, ref: FIRDatabaseReference) in
-				
-				completion?(e)
-		}
-	}
+    func delete(completion: ((Error?) -> Void)? = nil)
+    {
+        Database.database()
+            .reference(withPath: Self.COLLECTION_NAME)
+            .child(self.key)
+            .removeValue { (e: Error?, ref: DatabaseReference) in
+                
+                completion?(e)
+        }
+    }
 }
+
